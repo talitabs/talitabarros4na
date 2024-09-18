e0131c2c9910a76a51225f8d80d48358dab0686d
Talita Barros <35945808-bstalitabs@users.noreply.replit.com>
2024-09-18 23:56:21 +0000

    Modularizando com Models e Repositories

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,14 @@
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
 // Substitua pela sua string de conexão do Render.com
-const connectionString = 'postgresql://talita4na_user:j2vw3M3aLkEYBjphrA0UNfd8ccYrq1T0@dpg-cr7qs2ggph6c73en7an0-a.oregon-postgres.render.com/talita4na';
+const connectionString =
+  "postgresql://talita4na_user:j2vw3M3aLkEYBjphrA0UNfd8ccYrq1T0@dpg-cr7qs2ggph6c73en7an0-a.oregon-postgres.render.com/talita4na";
 
 const pool = new Pool({
   connectionString,
   ssl: {
     rejectUnauthorized: false, // Permite conexões SSL não autorizadas
-  }
+  },
 });
 
 export default pool;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
